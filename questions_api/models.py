84f84ba59eef55f57cdf13dfe6b7e4dba104cd68
Jessica Im <me@jessicaim.com>
2021-11-18 10:12:23 -0500

    working user model locally

--- questions_api/models.py
+++ questions_api/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question = models.CharField(max_length=100)
+
+class UserAccount(models.Model):
+    email = models.CharField(max_length=75, unique=True)
+    password = models.CharField(max_length=1000)
--- questions_api/serializers.py
+++ questions_api/serializers.py
@@ -1,7 +1,30 @@
 from rest_framework import serializers
 from .models import Question
+from .models import UserAccount
+
+from django.contrib.auth.hashers import make_password, check_password
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ('id', 'question')
+
+
+class UserAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAccount
+        fields = ('id', 'email', 'password')
+
+    def create(self, validated_data):
+        user = UserAccount.objects.create(
+        email = validated_data['email'],
+        password = make_password(validated_data['password'])
+        )
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        user = UserAccount.objects.get(email=validated_data["email"])
+        user.password = make_password(validated_data["password"])
+        user.save()
+        return user    
