eb4edc7f92cdba481f50b7dcd703b17cf46836d6
Yianni Catege <ycatege@gmail.com>
2021-11-18 16:53:12 -0500

    changed the models a bit

--- questions_api/models.py
+++ questions_api/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Comment(models.Model):
-    comment=models.CharField(max_length=(120))
-
-
 class Question(models.Model):
     question = models.CharField(max_length=100)
-    comment = models.ManyToManyField(Comment)
+
+class Comment(models.Model):
+    comment=models.CharField(max_length=(120))
+    question=models.ManyToManyField(Question)
 
 class UserAccount(models.Model):
     username = models.CharField(max_length=75, unique=True)
