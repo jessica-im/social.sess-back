34ec4f141623007e279a31451acd52d29d90affe
Jessica Im <me@jessicaim.com>
2021-11-18 11:58:49 -0500

    updated useraccount model to use username vs email

--- questions_api/models.py
+++ questions_api/models.py
@@ -5,5 +5,5 @@ class Question(models.Model):
     question = models.CharField(max_length=100)
 
 class UserAccount(models.Model):
-    email = models.CharField(max_length=75, unique=True)
+    username = models.CharField(max_length=75, unique=True)
     password = models.CharField(max_length=1000)
